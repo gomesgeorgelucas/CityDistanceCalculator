834fc527244abe7ea764590bdb5097258d5d2db6
George Lucas Gomes <gomes.georgelucas@gmail.com>
2022-12-18 09:57:49 -0400

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CityDistanceCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
